d6d74294f608da66a0e0b4fd1de73be11cffcdc0
K.Boopathi <44333589+programmerraja@users.noreply.github.com>
2021-04-15 09:27:29 +0530

    Updated the search options

--- controllers/search.js
+++ controllers/search.js
@@ -5,38 +5,12 @@ const User = require("../models/Users");
 
 async function post(req,res){
 	if(req.body){
-
 		 let {from,to,type,date}=req.body;
 		 type=type.toLowerCase();
-			 from=from.toLowerCase();
+		 from=from.toLowerCase();
 		 to=to.toLowerCase();
-		 //need to avoid duplicate in it 
-		 //using object to find the ride that not repeated O(n) time
-		 let temp={};
-		 let rides=[],ride;
-		 ride= await Ride.find({from:from,date:{"$gte":date},type:type});
-		
-	
-		 if(ride.length>0){
-		 	for(let i=0;i<ride.length;i++){
-		 		temp[ride[i]._id]="1";
-		 		rides.push(ride[i]);
-		 	}
-		 	
-		 }
-		 
-		 ride= await Ride.find({to:to,date:{"$gte": date},type:type});
-		 
-		if(ride.length>0){
-		 	for(let i=0;i<ride.length;i++){
-		 		//only push the ride if it it not already present
-		 		if(!temp[ride[i]._id]){
-		 			rides.push(ride[i]);
-		 		}
-		 	}
-		}
-		res.render("searchResult",{rides,search:{from,to,type,date},user:req.user});
-		return
+		 let rides= await Ride.find({from:from,to:to,date:{"$gte":date},type:type});
+		 res.render("searchResult",{rides,search:{from,to,type,date},user:req.user});
 	}
 	//say user to enter the search options
 }
